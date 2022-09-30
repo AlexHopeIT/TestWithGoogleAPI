20fa0b27ec7714d3ab89a3640f5c0d579ce74d92
AlexHopeIT <dexter4445@gmail.com>
2022-09-30 20:15:14 +0300

    Create Google API and connect with Sheet

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+import gspread
+
+path_to_keys = gspread.service_account(
+    filename='D:/Programs/pythonProject/Test for the Chanelservice/resources/keys.json')
+
+test_sheet = path_to_keys.open('Test for the Chanelservice')
+
 
