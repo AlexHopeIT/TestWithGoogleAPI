d7dd546bd73a03490336aa35d1326d86d5409ad6
AlexHopeIT <dexter44445@gmail.com>
2022-11-09 14:54:53 +0300

    The stage of filling the database

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import gspread as gr
 import requests
+import psycopg2
+from psycopg2 import Error
 from bs4 import BeautifulSoup
 
 # Connect to Google API
@@ -22,15 +24,70 @@ my_headers = {
 }
 responce = requests.get(url_cb, headers=my_headers)
 soup = BeautifulSoup(responce.content, 'html.parser')
-course_USD = soup.find('div', class_='col-md-2 col-xs-9 _right mono-num').getText(strip=True)
-current_course = course_USD[:7]
-current_course = float(current_course.replace(',', '.'))
+rate_USD = soup.find('div', class_='col-md-2 col-xs-9 _right mono-num').getText(strip=True)
+current_rate = rate_USD[:7]
+current_rate = float(current_rate.replace(',', '.'))
 
 # Convert USD/RUB
 values_col5 = []
 for price in values_col3[1:]:
-    price = float(price) * current_course
+    price = float(price) * current_rate
     values_col5.append(price)
 
-print(values_col5)
+# Changing the type of date
+correct_date = []
+for date in values_col4:
+    date = date.replace('.', '/')
+    correct_date.append(date)
+
+# Connect to DB
+try:
+    conn = psycopg2.connect(dbname='job_test', user='postgres',
+                            password='qwerty', port='5432',
+                            host='localhost')
+
+    cursor = conn.cursor()
+    cursor.execute('DROP TABLE IF EXISTS test')
+
+    # Create DB
+    create_table = '''CREATE TABLE "test"
+    ("№" INTEGER,
+    "заказ №" INTEGER,
+    "стоимость,$" INTEGER,
+    "срок поставки" TEXT,
+    "стоимость в руб." INTEGER);'''
+
+    cursor.execute(create_table)
+    conn.commit()
+
+    # Add values in DB
+    # for v1 in values_col1[1:]:
+    #     for v2 in values_col2[1:]:
+    #         for v3 in values_col3[1:]:
+    #             for v4 in correct_date[1:]:
+    #                 for v5 in values_col5:
+    #                     cursor.execute(f'INSERT INTO test VALUES ({v1}, {v2}, {v3}, {v4}, {v5})')
+    # for v1 in values_col1[1:]:
+    #     cursor.execute(f'INSERT INTO test ("№") VALUES ({v1})')
+    #     for v2 in values_col2[1:]:
+    #         cursor.execute(f'INSERT INTO test ("заказ №") VALUES ({v2})')
+    #         for v3 in values_col3[1:]:
+    #             cursor.execute(f'INSERT INTO test ("стоимость,$") VALUES ({v3})')
+    #             for v4 in correct_date[1:]:
+    #                 cursor.execute(f'INSERT INTO test ("срок поставки") VALUES ({v4})')
+    #                 for v5 in values_col5:
+    #                     cursor.execute(f'INSERT INTO test ("стоимость в руб.") VALUES ({v5})')
+
+    conn.commit()
+
+    table = cursor.execute('SELECT * FROM test')
+    print(table)
+
+except (Exception, Error) as error:
+    print("Error with PostgreSQL:", error)
+finally:
+    if conn:
+        conn.commit()
+        cursor.close()
+        conn.close()
 
